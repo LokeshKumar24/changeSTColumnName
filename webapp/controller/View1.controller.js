f70d43da6af7c20aa237d58e44f493fdf4c89726
LokeshKumar24 <Lokeshkumar24021996@gmail.com>
2021-07-03 12:07:57 +0530

    first commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("stable.controller.View1", {
+			onInit: function () {
+
+			},
+			changeColumnName:function(oEvent){
+				// debugger;
+				// var sTab=this.getView().byId("mySmartTable");
+				
+			//	var resouce=new sap.ui.model.resouce.ResourceModel()
+			}
+		});
+	});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"stable/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
